e2ac24f2b90db8191f4f7a7fecaefab4a2014eb3
xjq <xjq314@gmail.com>
2017-06-29 06:12:41 -0400

    Successful build#2

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 SMP PREEMPT Wed Jun 28 01:20:14 JST 2017"
+#define UTS_VERSION "#12 SMP PREEMPT Thu Jun 29 06:08:52 EDT 2017"
 #define LINUX_COMPILE_BY "xjq"
 #define LINUX_COMPILE_HOST "base2"
-#define LINUX_COMPILER "gcc version 4.7.4 (Ubuntu/Linaro 4.7.4-3ubuntu12) "
+#define LINUX_COMPILER "gcc version 4.8.5 (Ubuntu/Linaro 4.8.5-4ubuntu1) "
